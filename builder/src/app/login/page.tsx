9e863048224a34bb9d4b97b8372ea84928d1a921
steven.vos <steven.vos@iodigital.com>
2024-06-28 16:59:14 +0200

    feat: add styling to login flow

--- builder/src/app/login/page.tsx
+++ builder/src/app/login/page.tsx
@@ -2,23 +2,52 @@ import { login } from "@/actions/auth"
 
 export default function LoginPage() {
     return (
-        <form>
-            <label htmlFor="email">Email:</label>
-            <input
-                id="email"
-                name="email"
-                type="email"
-                required
-            />
-            <label htmlFor="password">Password:</label>
-            <input
-                id="password"
-                name="password"
-                type="password"
-                required
-            />
-            <button formAction={login}>Log in</button>
-            <a href="/register">Sign up</a>
-        </form>
+        <main className="flex min-h-screen items-center justify-center">
+            <form className="m-3 flex flex-col gap-4 rounded-md border p-6 lg:w-96">
+                <h1 className="text-2xl">blrplt builder</h1>
+                <div className="flex flex-col">
+                    <label
+                        htmlFor="email"
+                        className="text-sm"
+                    >
+                        Email
+                    </label>
+                    <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        className="rounded border p-2 transition-colors hover:border-neutral-600 focus:outline-black"
+                        required
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label
+                        htmlFor="password"
+                        className="text-sm"
+                    >
+                        Password
+                    </label>
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        className="rounded border p-2 transition-colors hover:border-neutral-600 focus:outline-black"
+                        required
+                    />
+                </div>
+                <button
+                    className="rounded border-2 border-black bg-black p-2 text-sm text-white transition-colors hover:bg-white hover:text-black focus:outline-dashed focus:outline-offset-2 focus:outline-black"
+                    formAction={login}
+                >
+                    Log in
+                </button>
+                <a
+                    className="mx-auto w-fit rounded p-1 text-sm hover:underline focus:outline-dashed focus:outline-offset-2 focus:outline-black"
+                    href="/register"
+                >
+                    Sign up
+                </a>
+            </form>
+        </main>
     )
 }
--- builder/src/app/register/page.tsx
+++ builder/src/app/register/page.tsx
@@ -1,24 +1,54 @@
 import { signup } from "@/actions/auth"
+import { sign } from "crypto"
 
 export default function RegisterPage() {
     return (
-        <form>
-            <label htmlFor="email">Email:</label>
-            <input
-                id="email"
-                name="email"
-                type="email"
-                required
-            />
-            <label htmlFor="password">Password:</label>
-            <input
-                id="password"
-                name="password"
-                type="password"
-                required
-            />
-            <button formAction={signup}>Sign up</button>
-
-        </form>
+        <main className="flex min-h-screen items-center justify-center">
+            <form className="m-3 flex flex-col gap-4 rounded-md border p-6 lg:w-96">
+                <h1 className="text-2xl">blrplt builder</h1>
+                <div className="flex flex-col">
+                    <label
+                        htmlFor="email"
+                        className="text-sm"
+                    >
+                        Email
+                    </label>
+                    <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        className="rounded border p-2 transition-colors hover:border-neutral-600 focus:outline-black"
+                        required
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label
+                        htmlFor="password"
+                        className="text-sm"
+                    >
+                        Password
+                    </label>
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        className="rounded border p-2 transition-colors hover:border-neutral-600 focus:outline-black"
+                        required
+                    />
+                </div>
+                <button
+                    className="rounded border-2 border-black bg-black p-2 text-sm text-white transition-colors hover:bg-white hover:text-black focus:outline-black focus:outline-offset-2 focus:outline-dashed"
+                    formAction={signup}
+                >
+                    Sign up
+                </button>
+                <a
+                    className="mx-auto w-fit text-sm hover:underline focus:outline-black focus:outline-offset-2 focus:outline-dashed rounded p-1"
+                    href="/login"
+                >
+                    Already have an account? Log in here!
+                </a>
+            </form>
+        </main>
     )
 }
