c5c6e12a67652d1303f5e4026cea23be89a285d5
steven.vos <steven.vos@iodigital.com>
2024-07-12 19:55:18 +0200

    feat: add defaultValue helper file for components

---
+++ builder/src/lib/components/defaultValues.ts
@@ -0,0 +1,16 @@
+export const componentDefaultValues = {
+    usps: {
+        title: "blrplt builder - unique selling points",
+        items: [
+            {
+                title: "blrplt builder - unique selling points",
+                description: "blrplt builder - unique selling points",
+                cta: {
+                    label: "blrplt builder - unique selling points",
+                    link: "blrplt builder - unique selling points",
+                    ariaLabel: "blrplt builder - unique selling points",
+                }
+            }
+        ]
+    }
+}
