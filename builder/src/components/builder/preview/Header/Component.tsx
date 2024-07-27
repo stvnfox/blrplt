5c8733c58441e5020c55eb25be0d721fc050b885
steven.vos <steven.vos@iodigital.com>
2024-07-27 17:18:24 +0200

    style: add padding to header component

--- builder/src/components/builder/preview/Header/Component.tsx
+++ builder/src/components/builder/preview/Header/Component.tsx
@@ -8,7 +8,7 @@ type HeaderProps = {
 
 export const HeaderComponent: FunctionComponent<HeaderProps> = ({ data }) => {
     return (
-        <header className="mt-16 text-center">
+        <header className="py-24 mt-16 text-center">
             <h1 className="mb-2 text-7xl">{data.title}</h1>
             <h2 className="mb-5 text-3xl">{data.subtitle}</h2>
             <p className="text-sm">{data.description}</p>
