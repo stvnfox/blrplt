db6ed63a2dbe89f4147774293afff33a45d80878
steven.vos <steven.vos@iodigital.com>
2024-08-03 16:21:13 +0200

    refactor: add new demo vid to landing page

--- client/components/pages/waiting/_components/Page.tsx
+++ client/components/pages/waiting/_components/Page.tsx
@@ -13,17 +13,39 @@ export interface HomePageProps {
 
 export function Page({ data, encodeDataAttribute }: HomePageProps) {
   // Default to an empty object to allow previews on non-existent documents
-  const { introduction = [], title = '', form = {} as IWaitingListForm, socials = [] as IWaitingListSocial[] } = data ?? {}
+  const {
+    introduction = [],
+    title = '',
+    form = {} as IWaitingListForm,
+    socials = [] as IWaitingListSocial[],
+  } = data ?? {}
 
   return (
     <div className="space-y-12">
       {/* Header */}
-      {title && form && <Header centered title={title} introduction={introduction} waitingList form={form} />}
+      {title && form && (
+        <Header
+          centered
+          title={title}
+          introduction={introduction}
+          waitingList
+          form={form}
+        />
+      )}
       {/* Socials */}
-      {socials.length > 0 && <SocialLinks items={socials}/>}
-      <hr className='w-11/12 md:w-[600px] mx-auto'/>
-      <div className='flex justify-center'>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/kxPJ1fKEl5g?si=ghkiX5pq5R3H2RA7" title="yt - blrplt builder" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      {socials.length > 0 && <SocialLinks items={socials} />}
+      <hr className="w-11/12 md:w-[600px] mx-auto" />
+      <div className="flex justify-center">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/h5GJFtl2Yow?si=OndZwBfDaB5dYCHE"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
       </div>
     </div>
   )
