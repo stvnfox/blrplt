f3688a63e81e7b1e9ec1c0734a73a758f592351f
steven.vos <steven.vos@iodigital.com>
2024-08-03 16:21:51 +0200

    refactor: changed title of iframe

--- client/components/pages/waiting/_components/Page.tsx
+++ client/components/pages/waiting/_components/Page.tsx
@@ -40,7 +40,7 @@ export function Page({ data, encodeDataAttribute }: HomePageProps) {
           width="560"
           height="315"
           src="https://www.youtube.com/embed/h5GJFtl2Yow?si=OndZwBfDaB5dYCHE"
-          title="YouTube video player"
+          title="blrplt builder demo"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           referrerPolicy="strict-origin-when-cross-origin"
