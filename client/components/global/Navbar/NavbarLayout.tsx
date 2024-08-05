b8e496f93e7b8f9386d6e7eb73d2b6365a051dad
steven.vos <steven.vos@iodigital.com>
2024-08-05 18:41:35 +0200

    feat: add logo icon and user icon with a link to builder

--- client/components/global/Navbar/NavbarLayout.tsx
+++ client/components/global/Navbar/NavbarLayout.tsx
@@ -1,3 +1,4 @@
+import { HandMetal, UserCircle } from 'lucide-react'
 import Link from 'next/link'
 
 import { resolveHref } from '@/sanity/lib/utils'
@@ -15,11 +16,23 @@ export default function Navbar(props: NavbarProps) {
 
   return (
     <div className="sticky top-0 z-10 flex flex-wrap items-center justify-between bg-white/80 px-4 py-4 backdrop-blur md:px-16 md:py-5 lg:px-32 container">
-      <Link href="/" className="text-lg hover:text-black md:text-xl focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-orange-600 focus-visible:outline-none rounded">
-        { title }
-      </Link>
+      <div className="flex justify-between items-center w-full">
+        <Link
+          href="/"
+          className="flex items-center gap-2 text-lg hover:text-black md:text-xl focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-orange-600 focus-visible:outline-none rounded"
+        >
+          <HandMetal /> {title}
+        </Link>
+        <Link
+          href="https://builder.blrplt.dev"
+          className="flex items-center gap-2 text-lg hover:text-orange-600 transition-colors md:text-xl focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-orange-600 focus-visible:outline-none rounded"
+        >
+          <UserCircle />
+        </Link>
+      </div>
       <nav className="flex items-center gap-x-5">
-        {menuItems && !isWaitingList &&
+        {menuItems &&
+          !isWaitingList &&
           menuItems.map((menuItem, key) => {
             const href = resolveHref(menuItem?.section)
             if (!href) {
