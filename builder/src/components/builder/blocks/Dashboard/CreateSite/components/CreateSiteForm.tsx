ced82556b136dfebdbf36c17685f0034910fa50f
steven.vos <steven.vos@iodigital.com>
2024-08-13 21:34:56 +0200

    refactor: changed style to template setting function

--- builder/src/components/builder/blocks/Dashboard/CreateSite/components/CreateSiteForm.tsx
+++ builder/src/components/builder/blocks/Dashboard/CreateSite/components/CreateSiteForm.tsx
@@ -12,7 +12,7 @@ import { Input } from "@/components/ui/input"
 import { useBuilderContext } from "@/providers/BuilderContextProvider"
 import { createUuid } from "@/lib/utils"
 import { componentDefaultValues } from "@/lib/components/defaultValues"
-import { addDefaultOpenGraphValues, addDefaultStyleSettingsValues } from "@/lib/settings/defaultValues"
+import { addDefaultOpenGraphValues, addDefaultTemplateSettingsValues } from "@/lib/settings/defaultValues"
 import { checkIfUrlIsAvailable } from "@/lib/urlCheck"
 
 const formSchema = z.object({
@@ -75,7 +75,7 @@ export function CreateSiteForm({ setOpen }: { setOpen: () => void }) {
             url: formattedUrl,
             id: createUuid(),
             userId: user,
-            settings: [{ style: addDefaultStyleSettingsValues() }, { openGraph: addDefaultOpenGraphValues() }],
+            settings: [{ template: addDefaultTemplateSettingsValues() }, { openGraph: addDefaultOpenGraphValues() }],
             pages: [
                 {
                     name: "Home",
