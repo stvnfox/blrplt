7a7626a5149beae016fa068fd0c0f279457e80e2
steven.vos <steven.vos@iodigital.com>
2024-09-18 08:22:28 +0200

    fix: add key to usps DesignerComponent

--- builder/src/components/builder/blocks/Dashboard/components/Usps/DesignerComponent.tsx
+++ builder/src/components/builder/blocks/Dashboard/components/Usps/DesignerComponent.tsx
@@ -22,9 +22,9 @@ export const DesignerComponent: FunctionComponent<UspsDesignerComponentProps> = 
                 <p className="text-balance text-xl text-primary md:text-3xl">{title}</p>
                 <p className="text text-balance text-baseText">{description}</p>
                 <div className="flex flex-wrap justify-center">
-                    {items.map((item) => {
+                    {items.map((item, index) => {
                         return (
-                            <div className="w-full p-4 lg:w-48">
+                            <div key={`usp-designer-${index}`} className="w-full p-4 lg:w-48">
                                 <div className="flex-grow rounded-xl border-2 border-neutral-200 border-primary p-3">
                                     <h2 className="mb-3 text-pretty text-sm font-medium text-primary">{item.title}</h2>
                                     <p className="text-sm leading-relaxed">{item.description}</p>
