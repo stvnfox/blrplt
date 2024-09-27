590b2637dac0666a2caa14168d2bbd2a7e3a77cf
steven.vos <steven.vos@iodigital.com>
2024-09-27 11:03:29 +0200

    refactor: removed unused comments/imports

--- builder/src/components/builder/dashboard/Dashboard/DesignerComponentWrapper/Component.tsx
+++ builder/src/components/builder/dashboard/Dashboard/DesignerComponentWrapper/Component.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react"
+import { useState } from "react"
 import { useDraggable, useDroppable } from "@dnd-kit/core"
 import { Trash2 } from "lucide-react"
 
@@ -9,9 +9,8 @@ import { useDesigner } from "@/lib/hooks/useDesigner"
 import { Button } from "@/components/ui/button"
 
 export const DesignerComponentWrapper = ({ component }: { component: ComponentElementInstance }) => {
-    const { removeComponent, setSelectedComponent, addedComponent } = useDesigner()
+    const { removeComponent, setSelectedComponent } = useDesigner()
     const [mouseIsOver, setMouseIsOver] = useState(false)
-    // const addedElementRef = useRef<HTMLDivElement>(null)
 
     const DesignerElement = PageDesignerComponents[component.type].designerComponent
 
@@ -58,12 +57,6 @@ export const DesignerComponentWrapper = ({ component }: { component: ComponentEl
         setSelectedComponent(null)
     }
 
-    // useEffect(() => {
-    //     if (addedComponent && addedComponent === component.id && addedElementRef.current) {
-    //         addedElementRef.current.scrollIntoView({ behavior: "smooth", block: "center" })
-    //     }
-    // }, [addedComponent])
-
     return (
         <div
             ref={draggable.setNodeRef}
@@ -103,10 +96,7 @@ export const DesignerComponentWrapper = ({ component }: { component: ComponentEl
                 </div>
             )}
             {topHalf.isOver && <div className="absolute h-2 w-full rounded-t-md bg-neutral-600" />}
-            <div
-                // ref={addedElementRef}
-                className={cn("rounded-md transition-opacity", mouseIsOver && "bg-neutral-600/20")}
-            >
+            <div className={cn("rounded-md transition-opacity", mouseIsOver && "bg-neutral-600/20")}>
                 <DesignerElement instance={component} />
             </div>
             {bottomHalf.isOver && <div className="absolute bottom-0 h-2 w-full rounded-b-md bg-neutral-600" />}
